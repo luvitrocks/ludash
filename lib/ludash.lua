4f6a0c88a62ea605dff2921e093fffa3c4e00254
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-08-22 16:28:02 +0300

    utility functions

--- lib/ludash.lua
+++ lib/ludash.lua
@@ -1,5 +1,3 @@
--- lib here
-
 local table = require('table')
 
 local lu = {}
@@ -15,6 +13,30 @@ end
 
 function lu.isArray (val)
 	return type(val) == 'table' and (val[1] or next(val) == nil)
+end
+
+function lu.isObject (val)
+	return type(val) == 'table'
+end
+
+function lu.isString (val)
+	return type(val) == 'string'
+end
+
+function lu.isNumber (val)
+	return type(val) == 'number'
+end
+
+function lu.isFunction (val)
+	return type(val) == 'function'
+end
+
+function lu.isBoolean (val)
+	return type(val) == 'boolean'
+end
+
+function lu.isNil (val)
+	return val == nil
 end
 
 return lu
